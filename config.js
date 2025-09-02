b892d8d0a1d4eba47eb3fdcf35dbc8f5e2ddbb0f
CryptoCapi <pnombre26@gmail.com>
2025-09-02 13:00:43 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,12 @@
 window.APP_CONFIG = window.APP_CONFIG || {};
 
+// Firebase config (usando SDK via CDN compat en index.html)
+window.APP_CONFIG.firebase = {
+  apiKey: "AIzaSyAOhLkpDUFZFgYposYPNjlJ2vSXqIXkBjA",
+  authDomain: "nitedcrypto.firebaseapp.com",
+  projectId: "nitedcrypto",
+  storageBucket: "nitedcrypto.firebasestorage.app", // no usado por ahora
+  messagingSenderId: "353115712912",
+  appId: "1:353115712912:web:819d03f1efb25db1331715",
+  measurementId: "G-3QKZ5L797Q" // opcional (Analytics)
+};
